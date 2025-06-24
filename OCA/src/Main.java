ed19630eeb23193595932d0c13b3b906dc6decc2
Luka Grujic <luka.grujic@edu.tbz.ch>
2025-06-24 09:28:49 +0200

    add small chages for frontend compatibllety

---
+++ OCA/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = new int[3];
+        nums[0] = 1;
+        nums[2] = 3;
+        System.out.println(nums[1]);
+    }
+}
